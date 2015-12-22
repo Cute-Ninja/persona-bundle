dd6cb4aba5131315c02242b9a22adfb75b544072
Ghriim <vignierquentin@yahoo.fr>
2015-12-22 18:29:03 +0100

    Update CuteNinjaPersonaBundle.php

--- CuteNinjaPersonaBundle.php
+++ CuteNinjaPersonaBundle.php
@@ -2,8 +2,21 @@
 
 namespace CuteNinja\PersonaBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class CuteNinjaPersonaBundle
+ *
+ * @package CuteNinja\PersonaBundle
+ */
 class CuteNinjaPersonaBundle extends Bundle
 {
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
 }

2fb9c192e9f6cc414d0e05f007444ad68e1c62e6
Ghriim <Ghriim@users.noreply.github.com>
2015-12-22 18:01:50 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cute_ninja_parabola');
+        $rootNode = $treeBuilder->root('cute_ninja_persona');
 
         return $treeBuilder;
     }

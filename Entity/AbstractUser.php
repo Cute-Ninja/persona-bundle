7a7c85ad9b7ff1d9352d6823dbfaab7c050c8009
Ghriim <vignierquentin@yahoo.fr>
2015-12-23 14:17:19 +0100

    Add user definition

---
+++ Entity/AbstractUser.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace CuteNinja\PersonaBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Class AbstractUser
+ *
+ * @package PersonaBundle\Entity
+ */
+abstract class AbstractUser implements UserInterface
+{
+    /**
+     * @var string $email
+     */
+    protected $email;
+
+    /**
+     * @var string $username
+     */
+    protected $username;
+
+    /**
+     * @var string $password
+     */
+    protected $password;
+
+    /**
+     * @var string[] Symfony roles
+     */
+    protected $roles;
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return $this
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     *
+     * @return $this
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     *
+     * @return $this
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getRoles()
+    {
+        return $this->roles ? : [];
+    }
+
+    /**
+     * @param string[] $roles
+     *
+     * @return $this
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSalt()
+    {
+        return;
+    }
+}
